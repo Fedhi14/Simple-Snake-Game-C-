2cead5140f38d90b32abfbf3f01cf18319fd7d96
yadesaML01 <yadurm9@gmail.com>
2025-06-09 22:32:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
-// this place is reserved for Yabets Mulu to Develop the main game loop to control the flow of the game.
+// this place is reserved for Yadesa Mulugeta to Develop the main game loop to control the flow of the game.
+// ------------------ utils.cpp ------------------
+#include "snake.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+int main() {
+    initializeGame();
+    
+    while (!gameOver) {
+        clearScreen();
+        printBoard();
+        getInput();
+        moveSnake();
+        checkCollision();
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    }
+
+    std::cout << "\nGame Over!\n";
+    if (playAgainPrompt()) {
+        initializeGame();
+        main(); 
+    }
+
+    return 0;
+}
