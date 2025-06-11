e0787d3a6a9163c987c8f3a1a3a2cd66fd9a9467
Metsnanat Awraris <metsnanatawraris@gmail.com>
2025-06-11 18:00:25 +0300

    Update snake.h

--- snake.h
+++ snake.h
@@ -8,3 +8,16 @@
 const int WIDTH = 40;
 const int HEIGHT = 20;
 
+enum Direction { UP, DOWN, LEFT, RIGHT };
+
+extern std::deque<std::pair<int, int>> snake;
+extern int direction;
+extern int score;
+extern bool gameOver;
+extern std::pair<int, int> fruit;
+
+// Game Logic
+void initializeGame();
+void moveSnake();
+void checkCollision();
+void generateFruit();
