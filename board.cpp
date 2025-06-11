4cbd693595fe7a4ed10abaf2757c6765c6cbff92
misiker0 <misikerdaniel@gmail.com>
2025-06-11 10:56:45 +0300

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -1,5 +1,5 @@
 // this place is leaved for Misiker Daniel to functions to display the game board using aesthetic borders.
 #include "snake.h"
 #include <iostream> 
-void printboard(){
+void printBoard(){
 
