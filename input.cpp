8ab0ae2a43ba9b1b6058220b315a491b8cee567d
yitnaw <yitinaw96bata@gmail.com>
2025-06-11 11:41:06 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -13,7 +13,7 @@ case 's' : if (direction != UP) direction = DOWN; break ;
 case 'a' : if (direction != RIGHT) direction = LEFT; break;
   
 case 'd' : if  (direction != LEFT) direction = RIGHT; break;
-     }
-   }
+      }
+    }
 }
 
