3490e8fd95b30216dfb533072520f8896bf21f91
yitnaw <yitinaw96bata@gmail.com>
2025-06-11 11:34:13 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -7,8 +7,11 @@ char ch = _getch();
 switch (ch) { 
  
 case 'w' : if (direction != DOWN) direction = UP; break ;
+  
 case 's' : if (direction != UP) direction = DOWN; break ;
+  
 case 'a' : if (direction != RIGHT) direction = LEFT; break;
+  
 case 'd' : if (direction != LEFT) direction = RIGHT; break;
      }
    }
