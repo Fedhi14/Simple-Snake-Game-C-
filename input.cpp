8fd71c605319c1bc5d725a36330efd87edf03d8b
yitnaw <yitinaw96bata@gmail.com>
2025-06-11 11:08:46 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -5,7 +5,7 @@ void getInput() {
   if (_kbhit () ) {
 char ch = _getch();
 switch (ch) { 
-  case 'w' : if (direction != DOWN) direction = UP; break ;
+ 
 case 's' : if (direction != UP) direction = DOWN; break ;
 case 'a' : if ( direction != RIGHT) direction = LEFT; break;
 case 'd' : if (direction != LEFT) direction = RIGHT; break;
