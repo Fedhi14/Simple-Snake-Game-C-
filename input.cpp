5f1bab167805e78c1797f11576f3f4376f41a94c
yitnaw <yitinaw96bata@gmail.com>
2025-06-11 11:02:12 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -3,12 +3,12 @@
 #include <conio.h>
 void getInput() {
   if (_kbhit () ) {
-char ch = _gEtch();
+char ch = _getch();
 switch (ch) { 
-  case 'w' : if (direction != DOWN) direction = UP; break ;
-case 's' : if (direction != UP) direction = DOWN; break ;
-case 'a' : if ( direction ! RIGHT) direction = LEFT; break;
-case 'd' : if (direction != LEFT) direction = RIGHT; break;
+  case 'w' ; if (direction != DOWN) direction = UP; break ;
+case 's' ; if (direction != UP) direction = DOWN; break ;
+case 'a' ; if ( direction ! RIGHT) direction = LEFT; break;
+case 'd' ; if (direction != LEFT) direction = RIGHT; break;
      }
    }
 }
