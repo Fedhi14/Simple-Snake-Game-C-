ac8189961f5bc968d5679a918675055f68c57470
Jabezz <yabimulu118@gmail.com>
2025-06-11 10:32:08 +0300

    Update snake.cpp
    Save original snake game configuration.
    
    Added "snake.h" for game headers.
    Included <deque> and <cstdlib> for utilities and snake body.
    Initialized snake as a deque of coordinates.
    Set direction to RIGHT, score to 0, and gameOver to false.
    Represented fruit as an ordered pair. Saving work in progress.

--- snake.cpp
+++ snake.cpp
@@ -1,1 +1,10 @@
 // this place is reserved for Yabets Mulu 
+#include "snake.h"
+#include <deque>
+#include <cstdlib>
+
+std::deque<std::pair<int, int>> snake;
+int direction = RIGHT;
+int score = 0;
+bool gameOver = false;
+std::pair<int, int> fruit;
